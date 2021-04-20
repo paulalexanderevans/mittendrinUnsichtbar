75cd06636b7bcfd3a7c16f6183e1a5f14e98e5ad
Paul Evans <paulalexanderevans@live.com>
2021-04-20 10:28:34 +0200

    logo smaller

---
+++ src/about.js
@@ -0,0 +1,59 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import React from "react";
+
+export default class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {}
+
+    render() {
+        return (
+            <div className="about">
+                <div className="aboutTxt">
+                    <h1>
+                        Der Gedanke an eine wohnungslose Person ergibt oft ein
+                        sehr stigmatisiertes Bild: Ein Mann mit abgetragener
+                        Kleidung, der auf der Straße lebt. Dieses vereinfachte
+                        Bild der sichtbaren Wohnungslosigkeit steht oft in
+                        Verbindung mit gesellschaftlichen Sehkonventionen und
+                        wird durch Medien zum Beispiel mit Porträts der
+                        Betroffenen verbreitet und verstärkt. Durch solch eine
+                        Verzerrung der Wahrnehmung bleiben viele Betroffene
+                        unbemerkt: Es sind Frauen, die aus Scham wegen ihrer
+                        Notlage und zum Schutz vor Gewalt unauffällig leben und
+                        ihre Wohnungslosigkeit verdeckt halten. Betroffene
+                        Frauen suchen meist Notübernachtungen auf, um einen
+                        sicheren Ort zum Schlafen zu finden. Tagsüber ist ihnen
+                        dieser Schutz nicht geboten, da sie die Einrichtungen
+                        frühmorgens verlassen müssen. Im Stadtbild fallen sie
+                        durch ihr optisches Erscheinungsbild nicht auf und
+                        existieren somit kaum in der gesellschaftlichen
+                        Wahrnehmung. Sie sind mittendrin – aber unsichtbar.
+                        Durch einen Perspektivenwechsel mittels Einwegkameras
+                        dokumentieren 13 wohnungslose Frauen ihr alltägliches
+                        Leben, um einen Einblick in ihre Lebenssituation zu
+                        geben. Sie bleiben auf Wunsch anonym und werden
+                        lediglich mit dem Anfangsbuchstaben ihres Vornamens
+                        abgekürzt. Die Suche nach den Frauen war mit sehr vielen
+                        Besuchen in verschiedenen Notübernachtungen in Berlin
+                        verbunden. Ich bedanke mich bei allen Leiterinnen für
+                        ihre hilfsbereite Unterstützung, die Einrichtungen
+                        mehrmals besuchen zu können. Christin Fritzsche
+                        Notübernachtung für Frauen Berlin-Mitte Isabelle
+                        Ponesicky Notübernachtung für Frauen Berlin-Moabit Elisa
+                        Lindemann Marie Berlin-Mitte Elke Loos Mitten im Kiez
+                        Berlin-Friedrichshain Natalie Kulik Evas Obdach
+                        Berlin-Neukölln Felina Beyer JugendKulturZentrum Pumpe
+                        Berlin-Mitte Mein besonderer Dank gilt allen Frauen für
+                        die Teilnahme an dem Projekt. Ich bedanke mich für ihre
+                        Offenheit, ihre Wärme und ihr Vertrauen.
+                    </h1>
+                </div>
+            </div>
+        );
+    }
+}
---
+++ src/kontakt.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default class Kontakt extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {}
+
+    render() {
+        return (
+            <div className="kontakt">
+                <div className="kontaktTxt">
+                    <h1>
+                        Das Projekt ist als Bachelorprojekt an der Universität
+                        der Künste Berlin in Visueller Kommunikation entstanden.
+                        <br></br>
+                        <br></br>
+                        Interessent*innen, Buchverlage, Sponsoren,
+                        Galerist*innen, Vereine:
+                        <br />
+                        Meldet Euch gerne für eine Zusammenarbeit, um den Frauen
+                        noch mehr Sichtbarkeit zu geben und über die Thematik
+                        mehr Aufmerksamkeit zu schaffen. <br></br>
+                        <br></br>Konzept & Gestaltung
+                        <br />
+                        Ronja Lang
+                        <br />
+                        <a href="mailto:r.lang@udk-berlin.de">
+                            r.lang@udk-berlin.de
+                        </a>
+                        <br></br>
+                        <br></br>Code
+                        <br />
+                        Paul Evans
+                        <br />
+                        <a href="mailto:paulalexanderevans@live.com">
+                            paulalexanderevans@live.com
+                        </a>
+                    </h1>
+                </div>
+            </div>
+        );
+    }
+}
